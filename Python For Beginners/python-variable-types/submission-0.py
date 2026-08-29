8ecd078e34a80c05f7803b2507af7bc018c8773e
Vikas Kumar <57791477+vikaskumar1187@users.noreply.github.com>
2026-08-29 16:55:34 +0530

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+integer_type = 10
+float_type = 10.00
+bool_type = True
+string_type = "10"
+list_type = ["hello", "vikas"]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
