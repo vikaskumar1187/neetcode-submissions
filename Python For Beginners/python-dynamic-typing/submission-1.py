62da4844673cb072316bc412e4eefb6e3e349d88
Vikas Kumar <57791477+vikaskumar1187@users.noreply.github.com>
2026-08-29 16:56:54 +0530

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.00
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "10"
+print(type(variable))
+variable = [10]
+print(type(variable))
